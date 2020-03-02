3971324d9c649bbbc7c0b2898830f8fd5c4f56b9
mr-and <souhei37@gmail.ru>
2020-03-02 22:01:05 +0300

    HW4 add Hibernate

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/d92eb0ca8123409685e4d112adc3fbfc)](https://www.codacy.com/manual/mr-and/topjava?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=mr-and/topjava&amp;utm_campaign=Badge_Grade)
+[![Build Status](https://travis-ci.org/mr-and/topjava.svg?branch=master)](https://travis-ci.org/JavaWebinar/topjava)
+
 Java Enterprise Online Project 
 ===============================
 Разработка полнофункционального Spring/JPA Enterprise приложения c авторизацией и правами доступа на основе ролей с использованием наиболее популярных инструментов и технологий Java: Maven, Spring MVC, Security, JPA(Hibernate), REST(Jackson), Bootstrap (css,js), datatables, jQuery + plugins, Java 8 Stream and Time API и хранением в базах данных Postgresql и HSQLDB.
--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
@@ -25,12 +25,12 @@ public class SpringMain {
 
             MealRestController mealController = appCtx.getBean(MealRestController.class);
             List<MealTo> filteredMealsWithExcess =
-                    mealController.getBetweenHalfOpen(
+                    mealController.getBetween(
                             LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
                             LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
             filteredMealsWithExcess.forEach(System.out::println);
             System.out.println();
-            System.out.println(mealController.getBetweenHalfOpen(null, null, null, null));
+            System.out.println(mealController.getBetween(null, null, null, null));
         }
     }
 }
